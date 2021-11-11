a9525d9363ca2eecbd0d9ac8f3625619e75746c2
Hayata Suenaga <hayata.suenaga@centre.edu>
2021-11-11 04:08:49 +0000

    Read db connection info from mysql.ini

--- html/config/db.php
+++ html/config/db.php
@@ -1,6 +1,15 @@
 <?php
 
   function connect($database = null) {
+    $config = parse_ini_file(__DIR__ . '/../../mysql.ini');
+    $dbname = 'pokemon';
+    $conn = new mysqli(
+      $config['mysqli.default_host'],
+      $config['mysqli.default_user'],
+      $config['mysqli.default_pw'],
+      $dbname);
+    /* Connect to the database */
+    if ($conn->connect_errno) die($conn->connect_error . "\n");
     /* Create Connection */
     $conn = mysqli_connect('localhost', 'hayata_suenaga', 'password', $database);
 
