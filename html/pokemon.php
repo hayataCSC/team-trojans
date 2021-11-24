4995756e47b464c500b99671582ce11490bd330f
Hayata Suenaga <hayata.suenaga@centre.edu>
2021-11-24 00:53:22 +0000

    Display pokemon info & Add move log form

--- html/pokemon.php
+++ html/pokemon.php
@@ -1,37 +1,58 @@
 <?php
+  /* Configure the error output level */
+  require(__DIR__ . '/config/error.php');
 
   /* Check if the query parameter is set */
   if (!isset($_GET['id']))
-    die('You need to specify the trainer\'s id in the query parameter');
+    die('You need to specify the pokemon\'s id in the query parameter');
 
-  /* Configure the error output level */
-  require(__DIR__ . '/config/error.php');
   /* Import the connect function from db.php */
   require(__DIR__ . '/config/db.php');
   /* Connect to the pokemon database */
   $conn = connect('pokemon');
 
-  /* Get the entry for the request trainer from the trainers table */
-  $query = 'SELECT * FROM trainer WHERE id = ?;';
+  /* Get the requested pokemon */
+  $query = 'SELECT name, species, level, is_female FROM pokemon WHERE id = ?;';
   $stmt = $conn->prepare($query);
   $stmt->bind_param('s', $_GET['id']);
   if (!$stmt->execute()) die();
   $result = $stmt->get_result();
-  /* Get the first row as an associative array */
-  $trainer = $result->fetch_assoc();
+  $pokemon = $result->fetch_all(MYSQLI_ASSOC)[0];
 
-  /* Get the entry for the request trainer from the trainers table */
-  $query = 'SELECT name, species, level, is_female FROM pokemon WHERE trainer_id = ?;';
-  $stmt = $conn->prepare($query);
-  $stmt->bind_param('s', $_GET['id']);
-  if (!$stmt->execute()) die();
-  $result = $stmt->get_result();
-  /* Get each row as an associative array */
-  $pokemons = $result->fetch_all(MYSQLI_ASSOC);
+  /* Get all pokemon moves */
+  $query = 'SELECT * FROM move';
+  $result = $conn->query($query);
+  if (!$result) die($conn->error);
+  $moves = $result->fetch_all(MYSQLI_NUM);
+  /* Get only move names */
+  $moves = array_map(function($move) { return $move[0]; }, $moves);
 
-  $query = "SELECT name FROM species";
-  $species = $conn->query($query)->fetch_all(MYSQLI_ASSOC);
   /* Close the connection to the database */
   $conn->close();
 
 ?>
+
+<!-- Import the header --->
+<?php require(__DIR__ . '/inc/header.php'); ?>
+
+<h1><?php echo $pokemon['name'] ?></h1>
+<h3><?php echo "Species: {$pokemon['species']}"; ?></h3>
+<h3><?php echo "Current level: {$pokemon['level']}"; ?></h3>
+<h3><?php echo 'Sex' . $pokemon['is_female'] ? 'Female' : 'Male'; ?></h3>
+
+<form action="/poke_care/api/moves.php" method="POST">
+  <input type="hidden" name="pokemon_id" value="<?php echo $_GET['pokemon_id']; ?>">
+  <div class="form-group">
+    <label for="moves">Move Learned</label>
+    <input list="movesList" id="move" name="move" class="form-control"/>
+    <datalist id="movesList">
+      <?php foreach($moves as $move): ?>
+        <option value="<?php echo $move; ?>">
+      <?php endforeach; ?>
+    </datalist>
+  </div>
+  <button type="submit" name="query" value="PUT">Log new move</button>
+</form>
+
+<!-- Import the footer --->
+<?php require(__DIR__ . '/inc/footer.php'); ?>
