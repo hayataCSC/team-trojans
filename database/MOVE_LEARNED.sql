21cd690f283b4ef9f2f15a330984c003acc1ae49
Hayata Suenaga <hayata.suenaga@centre.edu>
2021-11-24 00:32:52 +0000

    Store level instead of level_up_event_id

--- database/MOVE_LEARNED.sql
+++ database/MOVE_LEARNED.sql
@@ -1,17 +1,14 @@
 DROP TABLE IF EXISTS move_learned;
 CREATE TABLE move_learned (
-  PRIMARY KEY (event_id),
+  PRIMARY KEY (event_id, level),
   event_id INT,
   move_name VARCHAR(50),
-  level_up_event_id INT,
+  level INT,
   FOREIGN KEY (event_id)
     REFERENCES event(id)
     ON DELETE RESTRICT,
   FOREIGN KEY (move_name)
     REFERENCES move(name)
     ON UPDATE CASCADE
-    ON DELETE RESTRICT,
-  FOREIGN KEY (level_up_event_id)
-    REFERENCES level_up(event_id)
     ON DELETE RESTRICT
 );
