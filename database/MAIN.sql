bced68ca3c2be9264789ef229c97a2ab97ff5913
muhammad.mujtaba <muhammad.mujtaba@centre.edu>
2021-11-27 16:38:09 +0000

    aggregate data funcs added

--- database/MAIN.sql
+++ database/MAIN.sql
@@ -20,3 +20,24 @@ SOURCE procedures/befriend.sql;
 SOURCE procedures/get_friends.sql;
 SOURCE functions/get_partner.sql;
 SOURCE procedures/have_egg.sql;
+
+create function total_moves()
+Returns int
+Return(Select count(event_id) from move_learned);
+
+create function total_levelups()
+Returns int
+Return(Select count(event_id) from level_up);
+
+delimiter $$
+create Function total_eggs() RETURNS int
+Begin
+declare total int;
+declare actual_total int;
+set total = (Select count(event_id) from egg); 
+set actual_total = (Select total div 2);
+return actual_total;
+End $$
+delimiter ;
+
+
