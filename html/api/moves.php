ee84a350631e48a8939ac4346e4c56a2e48699fb
Hayata Suenaga <hayata.suenaga@centre.edu>
2021-11-28 23:23:30 +0000

    Delete extra comments

--- html/api/moves.php
+++ html/api/moves.php
@@ -23,15 +23,4 @@
   /* Redirect the user */
   header("Location: ../pokemon.php?id=$pokemonId");
   exit();
-
-  /* Prepare the query */
-  /* This query needs to insert the data to the event table as well as to the move_learned table.
-   * In order to insert a data into the move_learned table, you need the id created when inserting
-   * into the event table. The event table needs the time when the event took place.
-   * These two queries needs to happen in a single transaction. I might create a function that
-   * takes care of inserting data to these two tables. This function takes the following arguments:
-   * In order to insert a record to the move_learned table, I nedd the the latest level_up_event for
-   * the pokemon that just learned the new move. If there is already a move event that references
-   * the same level_up_event_id, the primary key constrain (or unique key constraint) to the
-   * level_up_event_id field should prevent the insertion. */
 ?>
