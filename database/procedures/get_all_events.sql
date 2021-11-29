00ef16417d72ed4fee19e43ed8d4279c25a57298
Hayata Suenaga <hayata.suenaga@centre.edu>
2021-11-29 00:03:12 +0000

    Get partner's name when getting egg events

--- database/procedures/get_all_events.sql
+++ database/procedures/get_all_events.sql
@@ -5,7 +5,7 @@ CREATE PROCEDURE get_all_events
   pokemon_id INT
 )
 BEGIN
-  SELECT ev.happened_at, partner_id, level_reached, move_name
+  SELECT ev.happened_at, l.level_reached, m.move_name, p.name AS partner_name
   FROM event ev
   LEFT JOIN egg eg
     ON ev.id = eg.event_id
@@ -13,6 +13,8 @@ BEGIN
     ON ev.id = l.event_id
   LEFT JOIN move_learned m
     ON ev.id = m.event_id
+  LEFT JOIN pokemon p
+    ON eg.partner_id = p.id
   WHERE ev.pokemon_id = pokemon_id
   /* The most recent event should come first */
   ORDER BY ev.happened_at DESC;
