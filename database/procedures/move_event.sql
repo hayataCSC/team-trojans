8392fc850d9a1ee1be83346caa27d99ef57f4232
Hayata Suenaga <hayata.suenaga@centre.edu>
2021-11-29 00:04:14 +0000

    Use function when getting pokemon's level

--- database/procedures/move_event.sql
+++ database/procedures/move_event.sql
@@ -5,26 +5,12 @@ CREATE PROCEDURE log_move_event(
   move_name VARCHAR(255)
 )
 BEGIN
-  /* Create a local variable for storing the current level of the pokemon */
-  DECLARE current_level INT;
-  /* Create a local variable for counting the number of move event records
-   * with specific attributes */
-  DECLARE count INT;
-  /* Get the current level of the pokemon */
-  SELECT level INTO current_level
-    FROM pokemon
-    WHERE id = pokemon_id;
-  /* Check if the same pokemon and level combination exists in the move_learned table */
-  SELECT COUNT(*) INTO count
-    FROM event e
-    INNER JOIN move_learned m
-      ON e.id = m.event_id
-    WHERE e.pokemon_id = pokemon_id AND m.level = current_level;
-  IF (count > 0) THEN
+  /* Check if the pokemon can learn new move */
+  IF (NOT can_learn_move(pokemon_id)) THEN
     SIGNAL SQLSTATE '45000'
       SET MESSAGE_TEXT = 'Pokemon can only learn a new move after leveling up';
   END IF;
-  /* Start a transaction */
+
   START TRANSACTION;
   /* Insert the pokemon's id and current datetime */
   INSERT INTO event (pokemon_id, happened_at)
@@ -32,8 +18,8 @@ BEGIN
   /* Insert the event id generated, the move's name, and the pokemon's
    * current level into the move_learned table */
   INSERT INTO move_learned (event_id, move_name, level)
-    VALUES(LAST_INSERT_ID(), move_name, current_level);
-  /* Commit the transactioin */
+    VALUES(LAST_INSERT_ID(), move_name, get_level(pokemon_id));
+
   COMMIT;
 END $$
 
