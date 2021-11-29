e25c87d5d781256fd36b6fe4001b6f5f977dbbeb
Hayata Suenaga <hayata.suenaga@centre.edu>
2021-11-29 00:04:58 +0000

    Import newly created sql scripts

--- database/MAIN.sql
+++ database/MAIN.sql
@@ -14,11 +14,15 @@ SOURCE EGG.sql;
 SOURCE LEVEL_UP.sql;
 SOURCE MOVE.sql;
 SOURCE MOVE_LEARNED.sql;
+
+SOURCE functions/get_level.sql;
+SOURCE functions/can_learn_move.sql;
+SOURCE functions/get_partner.sql;
+
 SOURCE procedures/move_event.sql;
 SOURCE procedures/level_up.sql;
 SOURCE procedures/befriend.sql;
 SOURCE procedures/get_friends.sql;
-SOURCE functions/get_partner.sql;
 SOURCE procedures/have_egg.sql;
 SOURCE procedures/get_all_events.sql;
 
